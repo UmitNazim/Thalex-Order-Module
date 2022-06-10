6355eec92ad0389952b225d36871db7b86575e74
Ümit nazım Çevik <cevikumitnazim@gmail.com>
2022-06-10 23:30:50 +0300

    axios implementing

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import './styles/main.scss'
 import App from './App.vue'
 import store from './store'
+import axios from './api'
 
-createApp(App).use(store).mount('#app')
+const app = createApp(App)
+app.config.performance = true
+app.config.globalProperties.$http = axios
+
+app.use(store).mount('#app')
