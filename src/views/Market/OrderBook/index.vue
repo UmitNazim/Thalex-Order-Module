1c4cd5b8ce9a20908e4294a8dce6f6e8738b4cc8
Ümit nazım Çevik <cevikumitnazim@gmail.com>
2022-06-11 18:09:05 +0300

    orderbook ui is done

--- src/views/Market/OrderBook/index.vue
+++ src/views/Market/OrderBook/index.vue
@@ -1,6 +1,63 @@
 <template>
   <organism-card hover class="market__orderbook">
-    <h3>Order Book</h3>
+    <div class="place-items-start gap-2">
+      <atom-icon icon="sell-buy" class="cursor-pointer" @on-click="displayItem('both')" />
+      <atom-icon icon="only-buy" class="cursor-pointer" @on-click="displayItem('buy')" />
+      <atom-icon icon="only-sell" class="cursor-pointer" @on-click="displayItem('sell')" />
+    </div>
+    <atom-divider class="my-8" />
+
+    <div class="overflow-hidden">
+      <table v-if="buy || both" class="w-100" style="border-collapse: collapse">
+        <thead>
+          <tr>
+            <td class="p-4 font-small t-oslo-grey">Price</td>
+            <td class="p-4 text-end font-small t-oslo-grey">Amount</td>
+            <td class="p-4 text-end font-small t-oslo-grey">Total</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="n in 15"
+            :key="n"
+            class="cursor-pointer b-watermelon-opacity"
+            style="
+              background: linear-gradient(to left, var(--watermelon-opacity) 75%, transparent 75%);
+            "
+          >
+            <td class="p-4 font-small t-watermelon">3.300</td>
+            <td class="p-4 font-small text-end t-french-grey">28.333</td>
+            <td class="p-4 font-small text-end t-french-grey">4040.65</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <div v-if="sell || both" class="mt-8 overflow-hidden">
+      <table class="w-100" style="border-collapse: collapse">
+        <thead>
+          <tr>
+            <td class="p-4 font-small t-oslo-grey">Price</td>
+            <td class="p-4 text-end font-small t-oslo-grey">Amount</td>
+            <td class="p-4 text-end font-small t-oslo-grey">Total</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="n in 15"
+            :key="n"
+            class="cursor-pointer"
+            style="
+              background: linear-gradient(to left, var(--vibrant-opacity) 75%, transparent 75%);
+            "
+          >
+            <td class="p-4 font-small t-vibrant">3.300</td>
+            <td class="p-4 font-small text-end t-french-grey">28.333</td>
+            <td class="p-4 font-small text-end t-french-grey">4040.65</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </organism-card>
 </template>
 
@@ -9,5 +66,12 @@ import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'OrderBook',
+  data: () => ({ sell: false, buy: false, both: true }),
+
+  methods: {
+    displayItem(val = 'both'): void {
+      ;['sell', 'both', 'buy'].forEach((element) => (this[element] = element === val))
+    },
+  },
 })
 </script>
